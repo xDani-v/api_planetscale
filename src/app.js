bc14eb4648eb3a5ae60a2ab27f8c22ea7121dc37
Daniel Villano <84256368+xDani-v@users.noreply.github.com>
2023-08-05 15:06:28 -0500

    agg cros

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { pool } from './db.js';
 import { PORT } from './config.js'
+import cors from 'cors'
+
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('GET request to the homepage');
